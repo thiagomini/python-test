12f86d3c69090c22b82b92f45dd5ebd4c1250ebd
Thiago Martins <thiago_valentim1@hotmail.com>
2024-03-21 09:54:55 -0300

    test: remove duplicate assertion

--- cached_function_test.py
+++ cached_function_test.py
@@ -5,4 +5,3 @@ def test_cached_function_returns_value():
     add1 = lambda x: x + 1
     add1_cached = CachedFunction(add1)
     assert add1_cached.calculate_function_value(2) == 3
-    assert add1_cached.calculate_function_value(2) == 3
