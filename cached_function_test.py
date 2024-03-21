ea922629e90c9a5dad522a023fc259bf19dfadc6
Thiago Martins <thiago_valentim1@hotmail.com>
2024-03-21 10:01:31 -0300

    test: ensure wrapped function is called only once

--- cached_function_test.py
+++ cached_function_test.py
@@ -5,3 +5,20 @@ def test_cached_function_returns_value():
     add1 = lambda x: x + 1
     add1_cached = CachedFunction(add1)
     assert add1_cached.calculate_function_value(2) == 3
+
+def test_cached_function_returns_cached_value():
+    # Arrange
+    def counter():
+        if not hasattr(counter, "calls"):
+            counter.calls = 0
+
+        counter.calls += 1
+        return counter.calls
+    counter_cached = CachedFunction(counter)
+    counter_cached.calculate_function_value()
+
+    # Act
+    times_executed = counter_cached.calculate_function_value()
+
+    # Assert
+    assert times_executed == 1
